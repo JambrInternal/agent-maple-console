edec76632d94f2fcb1774674dda1e98483d55a0e
Jeremy Legere <legere@fastmail.com>
2026-02-11 21:02:33 -0400

    feat: update service re-exports to include auth and token modules

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,4 +1,6 @@
 // Services re-exports
+export * from './auth';
+export * from './authEvents';
 export * from './organizations';
 export * from './projects';
 export * from './threads';
@@ -7,3 +9,5 @@ export * from './people';
 export * from './knowledge';
 export * from './insights';
 export * from './agentFacade';
+export * from './projectFacade';
+export * from './token';
